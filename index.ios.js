8c8168ba52153c0118d1a1028dc73780cc0a071f
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2015-08-10 21:43:37 +0300

    add project files

---
+++ index.ios.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var Main = require('./App/Components/Main');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  NavigatorIOS,
+  View
+} = React;
+
+class githubNotetaker extends React.Component{
+  render() {
+    return (
+      <NavigatorIOS style={styles.container}
+        initialRoute={{
+          title: 'Github Notetaker',
+          component: Main
+        }} />
+    );
+  }
+};
+
+var styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#111111'
+  }
+});
+
+
+AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
